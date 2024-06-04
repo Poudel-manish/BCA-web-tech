e5cc304d01c4a39729f184f5d91e71acb07c1e14
Manish poudel <Mr.manishpoudel@gmail.com>
2024-06-04 20:06:15 +0545

    project

---
+++ new project/vnf.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#define TRUE 1
+void main()
+{
+    char ch;
+    while (TRUE)
+    {
+        printf("Select your choice");
+        printf("1.Add\n2.Remove\n3. Inorder\n4. preorder \n5. postorder");
+        printf("n6. search\n7. Exit")
+        fflush(stdin);
+        ch=getch();
+        switch(ch)
+        {
+            case '1':break;
+            case'2':break;
+            case '3':break;
+            case '4':break;
+            case '5':break;
+            case '6':break;
+            case '7':break;
+            
+        }
+    }
+}
